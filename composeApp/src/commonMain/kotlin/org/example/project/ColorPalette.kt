5cc3335a08806aa9fa8e21c6c8780bc00e1fd1d3
Manfred Scheucher <manfred.scheucher1@gmail.com>
2025-10-17 10:41:56 +0200

    settings

--- composeApp/src/commonMain/kotlin/org/example/project/ColorPalette.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ColorPalette.kt
@@ -5,27 +5,27 @@ import androidx.compose.ui.graphics.Color
 // Palette generated using https://medialab.github.io/iwanthue/
 object ColorPalette {
     val colors = listOf(
-        "#cdf79e",
-        "#edddf6",
-        "#e1f433",
-        "#cee6f5",
-        "#9ffa5c",
-        "#fadcd9",
-        "#e7f158",
-        "#5df9ee",
-        "#e4f170",
-        "#9bf1ee",
-        "#d6f485",
-        "#c9f2e9",
-        "#7ffba7",
-        "#fde096",
-        "#62fbd2",
-        "#f2ed9c",
-        "#deefd6",
-        "#e0eead",
-        "#b9f3c2",
-        "#eaeac1"
-    ).map { Color(it.removePrefix("#").toLong(16) or 0x00000000FF000000) }
+        "#9deee5",
+        "#ecaa9a",
+        "#53c6ef",
+        "#e8e7b0",
+        "#74aff3",
+        "#a1bb7a",
+        "#c4b7ea",
+        "#8ac793",
+        "#eaaecf",
+        "#80d4ba",
+        "#8dc8f9",
+        "#b9d79e",
+        "#a1bde6",
+        "#bbbf8b",
+        "#55cdd8",
+        "#dcbe96",
+        "#8cd1e5",
+        "#bdefc5",
+        "#8dc3b8",
+        "#98c3a6"
+    ).map { Color((it.removePrefix("#").toLong(16) or 0xE6000000L).toInt()) }
 
     fun idToColor(id: Int): Color {
         if (colors.isEmpty()) {
