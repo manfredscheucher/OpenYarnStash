4aa9b43f2c4f7ec06e21949180feea7b170d392d
Manfred Scheucher <manfred.scheucher1@gmail.com>
2025-11-07 15:15:11 +0100

    localization + zip import backup

--- composeApp/src/commonMain/kotlin/org/example/project/ImageManager.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ImageManager.kt
@@ -3,26 +3,26 @@ package org.example.project
 class ImageManager(private val fileHandler: FileHandler) {
 
     suspend fun saveProjectImage(projectId: Int, image: ByteArray) {
-        fileHandler.writeBytes("img/project/$projectId.jpg", image)
+        fileHandler.writeBytes("img/project/${projectId}_1.jpg", image)
     }
 
     suspend fun getProjectImage(projectId: Int): ByteArray? {
-        return fileHandler.readBytes("img/project/$projectId.jpg")
+        return fileHandler.readBytes("img/project/${projectId}_1.jpg")
     }
 
     suspend fun deleteProjectImage(projectId: Int) {
-        fileHandler.deleteFile("img/project/$projectId.jpg")
+        fileHandler.deleteFile("img/project/${projectId}_1.jpg")
     }
 
     suspend fun saveYarnImage(yarnId: Int, image: ByteArray) {
-        fileHandler.writeBytes("img/yarn/$yarnId.jpg", image)
+        fileHandler.writeBytes("img/yarn/${yarnId}_1.jpg", image)
     }
 
     suspend fun getYarnImage(yarnId: Int): ByteArray? {
-        return fileHandler.readBytes("img/yarn/$yarnId.jpg")
+        return fileHandler.readBytes("img/yarn/${yarnId}_1.jpg")
     }
 
     suspend fun deleteYarnImage(yarnId: Int) {
-        fileHandler.deleteFile("img/yarn/$yarnId.jpg")
+        fileHandler.deleteFile("img/yarn/${yarnId}_1.jpg")
     }
 }
