ad7fa54074ed252156b347a199923668abe2605c
Manfred Scheucher <manfred.scheucher1@gmail.com>
2026-02-15 14:47:09 +0100

    unit tests not ready yet

--- composeApp/src/androidUnitTest/kotlin/org/example/project/ZipExportImportTestAndroid.kt
+++ composeApp/src/androidUnitTest/kotlin/org/example/project/ZipExportImportTestAndroid.kt
@@ -4,7 +4,8 @@ import java.io.ByteArrayInputStream
 
 actual fun createPlatformFileHandler(): FileHandler {
     // For unit tests, use JVM handler (Android instrumented tests would use real AndroidFileHandler)
-    return JvmFileHandler()
+    //return AndroidFileHandler()
+    throw NotImplementedError()
 }
 
 actual fun ByteArray.toInputStream(): Any {
